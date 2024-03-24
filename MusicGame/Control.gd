dff359a5262b53384d9f8d89ed0b324dce6b69e3
LuckyIRL <C23775565@mytudublin.ie>
2024-03-24 21:24:12 +0000

    MIDI controller not working

--- MusicGame/Control.gd
+++ MusicGame/Control.gd
@@ -1,21 +1,19 @@
 extends Control
 
 func _ready():
-	var midi_controller_scene = load("res://MainMenu/midi_Controller.tscn")
-	if midi_controller_scene == null:print("Failed to load MIDI controller scene")
+	var MIDI_controller_scene = load("res://MainMenu/MIDI_Controller.tscn")
+	if MIDI_controller_scene == null:
+		print("Failed to load MIDI controller scene")
+	else:
+		# Instance the loaded scene
+		var MIDI_controller_instance = MIDI_controller_scene.Instance()
 
+		# Add the instanced scene as a child of this node
+		add_child(MIDI_controller_instance)
 
+		# Access the slider nodes
+		var _frequency_slider = MIDI_controller_instance.get_node("MarginContainer/HBoxContainer/FrequencySlider")
+		var _pitch_slider = MIDI_controller_instance.get_node("MarginContainer/HBoxContainer/PitchSlider")
+		var _tempo_slider = MIDI_controller_instance.get_node("MarginContainer/HBoxContainer/TempoSlider")
 
-
-	# Instance the loaded scene
-	var midi_controller_instance = midi_controller_scene.instance()
-
-	# Add the instanced scene as a child of this node
-	add_child(midi_controller_instance)
-
-	# Access the slider nodes
-	var _frequency_slider = midi_controller_instance.get_node("MarginContainer/HBoxContainer/FrequencySlider")
-	var _pitch_slider = midi_controller_instance.get_node("MarginContainer/HBoxContainer/PitchSlider")
-	var _tempo_slider = midi_controller_instance.get_node("MarginContainer/HBoxContainer/TempoSlider")
-
-	# Now you can use these slider nodes to connect signals or manipulate their properties
+		# Now you can use these slider nodes to connect signals or manipulate their properties
--- MusicGame/MainMenu/main_menu.gd
+++ MusicGame/MainMenu/main_menu.gd
@@ -3,14 +3,16 @@ extends Control
 
 @onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/StartButton as Button
 @onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/ExitButton as Button
-@onready var start_level = preload("res://MusicGame.tscn") as PackedScene
+@onready var start_level = preload("res://MainMenu/MIDI_Controller.tscn") as PackedScene
 
 func _ready():
 	start_button.button_down.connect(on_start_pressed)
 	exit_button.button_down.connect(on_exit_pressed)
 	
-func on_start_pressed() ->void:
-	get_tree().change_scene_to_packed(start_level)
-	
-func on_exit_pressed() ->void:
+func on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://MainMenu/MIDI_Controller.tscn")
+
+func on_exit_pressed() -> void:
 	get_tree().quit()
+
+   
